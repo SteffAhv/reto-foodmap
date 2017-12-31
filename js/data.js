d13128e8b64f4b278e4f5ba65e5273954adddca5
SteffAhv <tefavv22@gmail.com>
2017-12-31 01:31:03 -0500

    accediendo a la data vista restaurants

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-// Implementamos la data con cinco propiedades que vendrian a ser los filtros de comida
+// Implementamos la data con cinco keys principales que vendrian a ser los filtros 
 var data = {
     'PERUVIAN': {
       'casa-mia': {
@@ -19,47 +19,55 @@ var data = {
         'name': 'Panda Garden',
         'address': 'Av. Morales Nro 4554',
         'about': 'You will find delicius food, come an visit us.',
+        'picture' : '../assets/img/japanese1.jpg',
       },
       'naruto': {
         'name': 'Naruto Restaurant',
         'address': 'Av. Canada Nro 1154',
         'about': 'You will find delicius japanese food, enjoy it.',
+        'picture' : '../assets/img/japanese2.jpg',
       }
     },
     'ITALIAN': {
       'gustoes': {
         'name': 'Gustoes Restaurant',
         'address': 'Roman Street  Nro 8575-A',
-        'about': 'You will find delicius food, come an visit us.'
+        'about': 'You will find delicius food, come an visit us.',
+        'picture' : '../assets/img/italian1.jpg'
       },
       'ratatouille': {
         'name': 'Ratatouille Restaurant',
         'address': 'Chelseas Avenue Nro 4734',
-        'about': 'You will find delicius food, come an visit us.'
+        'about': 'You will find delicius food, come an visit us.',
+        'picture' : '../assets/img/italian4.jpg'
       }
     },
     'THAI': {
       'honk-kai': {
         'name': 'Honk Kai Restaurant',
         'address': 'Av. Buenos Aires Nro 7364',
-        'about': 'You will find delicius food, come an visit us.'
+        'about': 'You will find delicius food, come an visit us.',
+        'picture' : '../assets/img/thai1.jpg'
       },
       'kai-zen': {
         'name': 'Kai-Zen Thailand Food',
         'address': 'Texas Avenue Nro 363',
-        'about': 'Try our delicious buffet, you will not regret.'
+        'about': 'Try our delicious buffet, you will not regret.',
+        'picture' : '../assets/img/thai4.jpg',
       }
     },
     'FAST': {
       'chessecake': {
         'name': 'Chesse Cake Factory',
         'address': 'Pasadena Avenue Nro 7364',
-        'about': 'You will find delicius food, come an visit us.'
+        'about': 'You will find delicius food, come an visit us.',
+        'picture' : '../assets/img/fast1.jpg',
       },
       'brosters': {
         'name': 'Brosters Grill',
         'address': 'Canada Street Nro 3837-B',
-        'about': 'You will find delicius food, come an visit us.'
+        'about': 'You will find delicius food, come an visit us.',
+        'picture' : '../assets/img/fast5.jpg',
       }
     }
 };
